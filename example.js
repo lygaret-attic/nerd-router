e8c9c77e45cdd401269296743543b9af8253b50a
Jon Raphaelson <lygaret@gmail.com>
2010-06-30 18:45:24 -0600

    moved to single word nerdrouter

--- example.js
+++ example.js
@@ -1,35 +1,40 @@
 var http        = require('http'),
 	url         = require('url'),
-	nerdrouter  = require('nerd-router'),
-	logger      = require('nerd-router/logger');
+	nerdrouter  = require('nerdrouter'),
+	logger      = require('nerdrouter/logger');
 
 var router = nerdrouter.route({
 
 	'get /': function(path, req, resp) {
-		resp.writeHeader(200, {'Content-Type': 'text/plain'});
+		resp.writeHead(200, {'Content-Type': 'text/plain'});
 		resp.end("You got the home page\n");
+	},
+
+	'get /:num{\\d{1,3}}/': function(path, req, resp, num) {
+		resp.writeHead(200, {'Content-Type': 'text/plain'});
+		resp.end("You asked for item #" + num);
 	},
 
 	'get /jon/*': {
 	
 		'get /': function(path, req, resp) {
-			resp.writeHeader(200, {'Content-Type': 'text/plain'});
+			resp.writeHead(200, {'Content-Type': 'text/plain'});
 			resp.end("You got jon's home page\n");
 		},
 
 		'get /:name{@slug}': function(path, req, resp, slug) {
-			resp.writeHeader(200, {'Content-Type': 'text/plain'});
+			resp.writeHead(200, {'Content-Type': 'text/plain'});
 			resp.end("Jon says: 'Hi, " + slug + "!'\n");
 		}
 	},
 
 	'get /:year{@year}/:month{@month}/:day{@day}': function(path, req, resp, year, month, day) {
-		resp.writeHeader(200, {'Content-Type': 'text/plain'});
+		resp.writeHead(200, {'Content-Type': 'text/plain'});
 		resp.end("It was " + month + "/" + day + "/" + year + ", the worst day ever.\n");	
 	},
 
 	'get *': function(path, req, resp) {
-		resp.writeHeader(404, {'Content-Type': 'text/plain'});
+		resp.writeHead(404, {'Content-Type': 'text/plain'});
 		resp.end("'" + path + "' was not found. 404.\n");
 	}
 
